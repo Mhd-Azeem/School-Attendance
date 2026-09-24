dc3df3de35357d83e533cd008b87a262ce373afc
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-24 21:46:36 +0530

    Make web app embeddable in Original APK

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './AuthContext'
 import './styles.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter basename={import.meta.env.BASE_URL}><AuthProvider><App/></AuthProvider></BrowserRouter></React.StrictMode>)
-if('serviceWorker'in navigator&&import.meta.env.PROD)window.addEventListener('load',()=>navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`))
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><AuthProvider><App/></AuthProvider></HashRouter></React.StrictMode>)
+if(location.protocol.startsWith('http')&&'serviceWorker'in navigator&&import.meta.env.PROD)window.addEventListener('load',()=>navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`))
 
