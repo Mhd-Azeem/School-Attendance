8d4e487fe12e1a6a891ae481e3fa49d8c2f8d159
Mhd-Azeem <azeemzahira111@gmail.com>
2026-09-26 14:50:15 +0530

    Load success modal styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './AuthContext'
 import './styles.css'
+import './success-modal.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><HashRouter><AuthProvider><App/></AuthProvider></HashRouter></React.StrictMode>)
 if(location.protocol.startsWith('http')&&'serviceWorker'in navigator&&import.meta.env.PROD)window.addEventListener('load',()=>navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`))
 
